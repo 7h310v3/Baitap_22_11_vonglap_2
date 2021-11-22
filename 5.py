c7490cf98fba85487c8e1d446ec25d81845d58d5
7h310v3 <minh92dd@gmail.com>
2021-11-22 16:07:41 +0700

    thelove

--- 5.py
+++ 5.py
@@ -1,22 +1,28 @@
 num = []
+arr = []
 
-#nhập dữ liệu cho mảng
-def enter_num():
-    global num
-    print('Nhập dữ liệu xong thì nhập (Done) hoặc (done)')
-    while True:
-        x = input('Dữ liệu cần nhập cho mảng: ').upper()
-        if x == 'STOP':
-            print('Dữ liệu đã nhập thành công')
-            break
-        num.append(float(x))
-
-
-
-
-
-#chạy chương trình
-enter_num()
-
-print("Mảng trước khi sắp xếp")
+#main
+while True:
+    n = input('Dữ liệu cần nhập cho mảng: ')
+    if (n.isdigit() == True):
+        num.append(int(n))
+    else:
+        print('Dữ liệu đã nhập thành công')
+        break
 print(num)
+
+x = int(input("Nhập số cần tìm: "))
+
+count = 0
+flag = 0
+while(count < len(num)):
+        if(x == num[count]):
+            arr.append(count)
+            flag = 1
+        count += 1
+
+if(flag == 1):
+    print("Giá trị", x, "xuất hiện đầu tiên trong danh sách tại vị trí:",arr[0])
+    print("Giá trị", x, "xuất hiện trong danh sách tại các vị trí:",arr)
+else:
+    print("Không có giá trị", x, "xuất hiện trong danh sách")
